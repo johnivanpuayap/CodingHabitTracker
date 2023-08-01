48800c7b76d9e112e485f766ee831783b53a2720
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-08-01 16:48:52 +0800

    Add a Command Line Interface

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import datetime
 import requests
 import os
 
-# Change this when you want to add new data (can be in minutes)
-NO_OF_MINUTES = 200
-
-# Data
+# Config Data
 USERNAME = 'johnivanpuayap'
 USER_TOKEN = os.environ['PIXELA_TOKEN']
+GRAPH_ID = 'codingtracker'
+
+# Data
+NO_OF_MINUTES = 0
 DATE_TODAY = datetime.datetime.now().strftime("%Y%m%d")
-GRAPH_ID = 'graph1'
 
 PIXELA_ENDPOINT = 'https://pixe.la/v1/users'
 GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
@@ -17,57 +17,65 @@ POST_ENDPOINT = f"{GRAPH_ENDPOINT}/{GRAPH_ID}"
 UPDATE_ENDPOINT = f"{POST_ENDPOINT}/{DATE_TODAY}"
 DELETE_ENDPOINT = UPDATE_ENDPOINT
 
-create_user_parameters = {
-    "token": USER_TOKEN,
-    "username": USERNAME,
-    "agreeTermsOfService": "yes",
-    "notMinor": "yes",
-}
-
-# # Create a User
-# response = requests.post(url=PIXELA_ENDPOINT, json=create_user_parameters)
-# print(response.text)
-
-# Create a Graph Definition
-
-
-GRAPH_ID = "graph1"
-
-graph_parameters = {
-    "id": GRAPH_ID,
-    "name": "Coding Hours Tracker",
-    "unit": "hour",
-    "type": "float",
-    "color": "sora",
-}
-
 headers = {
     "X-USER-TOKEN": USER_TOKEN
 }
 
-# response = requests.post(url=GRAPH_ENDPOINT, headers=headers, json=graph_parameters)
-# print(response.text)
 
-# # Post a Pixel in a Graph
+def create_user():
+    create_user_parameters = {
+        "token": USER_TOKEN,
+        "username": USERNAME,
+        "agreeTermsOfService": "yes",
+        "notMinor": "yes",
+    }
 
-#
-# post_parameters = {
-#     "date": DATE_TODAY,
-#     "quantity": f"{NO_OF_MINUTES/60}",
-# }
-#
-# response = requests.post(url=POST_ENDPOINT, headers=headers, json=post_parameters)
-# print(response.text)
+    response = requests.post(url=PIXELA_ENDPOINT, json=create_user_parameters)
+    print(response.text)
 
-# Update a Pixel
-#
-#
-# update_parameters = {
-#     "quantity": f"{NO_OF_MINUTES/60}"
-# }
-#
-# response = requests.put(url=UPDATE_ENDPOINT, headers=headers, json=update_parameters)
 
-# Delete a Pixel
-response = requests.delete(url=DELETE_ENDPOINT, headers=headers)
-print(response.text)
+def create_graph():
+    graph_parameters = {
+        "id": GRAPH_ID,
+        "name": "Coding Hours Tracker",
+        "unit": "hours",
+        "type": "float",
+        "color": "sora",
+        "timezone": "Asia/Manila"
+    }
+
+    response = requests.post(url=GRAPH_ENDPOINT, headers=headers, json=graph_parameters)
+    print(response.text)
+
+
+def post_pixel():
+    # Post a Pixel in a Graph
+
+    post_parameters = {
+        "date": DATE_TODAY,
+        "quantity": f"{NO_OF_MINUTES / 60}",
+    }
+
+    response = requests.post(url=POST_ENDPOINT, headers=headers, json=post_parameters)
+
+    if response.json()['isSuccess']:
+        print(f"Added{NO_OF_MINUTES / 60: .2f} hours today")
+
+
+def update_pixel():
+    update_parameters = {
+        "quantity": f"{NO_OF_MINUTES / 60}"
+    }
+
+    response = requests.put(url=UPDATE_ENDPOINT, headers=headers, json=update_parameters)
+    print(response.text)
+
+
+def delete_pixel():
+    response = requests.delete(url=DELETE_ENDPOINT, headers=headers)
+    print(response.text)
+
+
+print(f"Welcome, {USERNAME}")
+NO_OF_MINUTES = int(input("How many minutes did you code today?: "))
+post_pixel()
