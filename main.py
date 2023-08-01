a3fb52a92cc2e60e068b76983d632950621535ea
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-08-01 15:57:52 +0800

    Create a Graph

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
+import datetime
+
 import requests
 import os
 
-# DATA
-PIXELA_ENDPOINT = 'https://pixe.la/v1/users'
+# Data
+USERNAME = 'johnivanpuayap'
 USER_TOKEN = os.environ['PIXELA_TOKEN']
+PIXELA_ENDPOINT = 'https://pixe.la/v1/users'
 
 create_user_parameters = {
     "token": USER_TOKEN,
-    "username": "johnivanpuayap",
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
-# # CREATE A USER
+# # Create a User
 # response = requests.post(url=PIXELA_ENDPOINT, json=create_user_parameters)
+# print(response.text)
+
+# Create a Graph Definition
+
+GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+GRAPH_ID = "graph1"
+
+graph_parameters = {
+    "id": GRAPH_ID,
+    "name": "Coding Hours Tracker",
+    "unit": "hour",
+    "type": "float",
+    "color": "sora",
+}
+
+headers = {
+    "X-USER-TOKEN": USER_TOKEN
+}
+
+response = requests.post(url=GRAPH_ENDPOINT, headers=headers, json=graph_parameters)
+print(response.text)
 
